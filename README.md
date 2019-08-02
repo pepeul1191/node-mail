652a20fcef870cf389606f643134d6deb5180c9a
Pepe Valdivia <jvaldivia@softweb.pe>
2019-08-02 08:12:45 -0500

    usando ejs para templates

--- README.md
+++ README.md
@@ -13,3 +13,5 @@ Fuentes:
 + https://stackoverflow.com/questions/33344908/how-to-use-nodemailer-to-send-mail-from-a-node-js-app
 + https://myaccount.google.com/security?pli=1
 + https://stackoverflow.com/questions/6317169/using-gitignore-to-ignore-but-not-delete-files
++ https://stackoverflow.com/questions/38890752/render-ejs-template-and-save-it-as-a-file
++ https://ejs.co/#install
--- app.js
+++ app.js
@@ -1,18 +1,19 @@
 const mail = require('./client'); 
 const ejs = require('ejs');
+const fs   = require('fs');
 
-
-function template(data){
-  var str = '<h1>That was easy! <%= name + " " + new Date() %></h1>';
-  return ejs.render(str, data);
+function makeMail(data){
+  var template = fs.readFileSync('./templates/demo.ejs', 'utf-8');
+  return ejs.render(template, data);
 }
 
 var mailOptions = {
   from: 'demo@softweb.pe',
   to: 'pepe.valdivia.caballero@gmail.com', //, jovaldiv@ulima.edu.pe, jvaldivia@softweb.pe',
   subject: 'Sending Email using Node.js',
-  html: template({
+  html: makeMail({
     name: 'Pepe',
+    title: 'Hola mundo',
   })
 };
 
