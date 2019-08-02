4ffc0668d5c56491b5e44e0a4a33cd6a935587e4
Pepe Valdivia <jvaldivia@softweb.pe>
2019-08-02 08:05:24 -0500

    Usando EJS para templates

--- app.js
+++ app.js
@@ -1,10 +1,19 @@
-var mail = require('./client'); 
+const mail = require('./client'); 
+const ejs = require('ejs');
+
+
+function template(data){
+  var str = '<h1>That was easy! <%= name + " " + new Date() %></h1>';
+  return ejs.render(str, data);
+}
 
 var mailOptions = {
   from: 'demo@softweb.pe',
   to: 'pepe.valdivia.caballero@gmail.com', //, jovaldiv@ulima.edu.pe, jvaldivia@softweb.pe',
   subject: 'Sending Email using Node.js',
-  html: '<h1>That was easy!</h1>'
+  html: template({
+    name: 'Pepe',
+  })
 };
 
 async function enviar(){
