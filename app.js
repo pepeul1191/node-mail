fdea0512b67b8ee8efedfe6a24aa52206d708591
Pepe Valdivia <jvaldivia@softweb.pe>
2019-08-02 22:21:39 -0500

    Usando express

--- app.js
+++ app.js
@@ -1,6 +1,23 @@
-const mail = require('./client'); 
 const ejs = require('ejs');
 const fs   = require('fs');
+const express = require('express');
+const app = express();
+const mail = require('./client'); 
+
+app.get('/sails/wellcome', function (req, res) {
+  // query
+  var data = {
+    name: 'sails',
+    title: 'Hola mundo',
+  };
+  var template = fs.readFileSync('./templates/sails/demo.ejs', 'utf-8');
+  var html = ejs.render(template, data)
+  res.send(html);
+});
+
+app.listen(3000, function () {
+  console.log('Example app listening on port 3000!');
+});
 
 function makeMail(data){
   var template = fs.readFileSync('./templates/demo.ejs', 'utf-8');
@@ -27,4 +44,4 @@ async function enviar(){
   console.log(response);
 }
 
-enviar();
+// enviar();
